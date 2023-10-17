d1a9b579ea89f4fe401334f14a7e5c15826ef41e
Ilkin Hamzayev <ilkinhamzayev925@gmail.com>
2023-10-17 14:47:55 +0200

    completed the function to test one single round

--- index.js
+++ index.js
@@ -2,4 +2,33 @@
 
 // Rock Paper And Scissors Game played on Console
 
-getComputerChoice()
+function getComputerChoice(){
+    let options = [
+        'Rock',
+        'Paper',
+        'Scissors'
+    ]
+
+    let randomNumber = Math.floor(Math.random()*options.length);
+    console.log(options[randomNumber]);
+}
+
+// getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if(computerSelection > playerSelection){
+     }
+    
+     return "You Lose! Paper beats Rock"
+
+
+  }
+
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection))
+
